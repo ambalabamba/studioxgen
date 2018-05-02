fb78e544b8e69fdba49a6850386b344a67d50fa6
ambalabamba <38856804+ambalabamba@users.noreply.github.com>
2018-05-02 13:29:06 +0200

    Update schallywag.py

--- schallywag.py
+++ schallywag.py
@@ -3,6 +3,6 @@ import requests
 import random
 
 def spam():
-    threading.Timer(0.001, spam).start()
+    threading.Timer(0.01, spam).start()
     requests.get("http://api.xgenstudios.com/?method=xgen.stats.get&username=weathernowdays")
 spam()
